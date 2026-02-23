9ee75bfb4977722419625f6004b0d156fbe6a4b7
dungnn <trandung070899@gmail.com>
2026-02-23 22:17:33 +0700

    add complete function remove todo from list with pop()

--- 2.Todo-app/main.py
+++ 2.Todo-app/main.py
@@ -1,19 +1,49 @@
-prompt = 'Type add, show or exit:'
+prompt = """Enter command:
+1. add
+2. edit
+3. show
+4. complete
+5. exit
+"""
 
+class CMD:
+    ADD = '1'
+    EDIT = '2'
+    SHOW = '3'
+    COMPLETE = '4'
+    EXIT = '5'
 todos = []
-while True:
 
+def add_todo():
+    todo = input('enter a todo:')
+    todos.append(todo)
+
+def edit_todos():
+    to_do_id = int(input('enter a id to edit: '))
+    todo = input('enter new todo:')
+    todos[to_do_id - 1] = todo
+
+def show_todos():
+    for index, todo in enumerate(todos, start=1):
+        print(f"{index}-{todo.title()}")
+
+def complete_todos():
+    to_do_id = int(input('enter a id to edit: '))
+    complete = todos.pop(to_do_id - 1)
+    print(f"completed: {complete}")
+    print("Left todo:")
+    show_todos()
+
+while True:
     action = input(prompt).strip()
     match action:
-        case 'add':
-            todo = input('enter a todo:')
-            todos.append(todo)
-        case 'show':
-            for index, todo in enumerate(todos, start=1):
-                print(f"{index}-{todo.title()}")
-        case 'edit':
-            to_do_id = int(input('enter a id to edit: '))
-            todo  = input('enter new todo:')
-            todos[to_do_id - 1] = todo
-        case 'exit':
+        case CMD.ADD:
+            add_todo()
+        case CMD.EDIT:
+            edit_todos()
+        case CMD.SHOW:
+            show_todos()
+        case CMD.COMPLETE:
+            complete_todos()
+        case EXIT:
             break
