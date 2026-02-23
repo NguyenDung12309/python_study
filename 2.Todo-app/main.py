755b13e0f270f2b411e6eabd8bb0059017ca0d36
dungnn <trandung070899@gmail.com>
2026-02-23 20:30:34 +0700

    implement add, show, edit to-to App

--- 2.Todo-app/main.py
+++ 2.Todo-app/main.py
@@ -1,5 +1,19 @@
-prompt = 'Enter a todo:'
-todo_1 = input(prompt)
-todo_2 = input(prompt)
-todos = [todo_1, todo_2]
-print(todos)
+prompt = 'Type add, show or exit:'
+
+todos = []
+while True:
+
+    action = input(prompt).strip()
+    match action:
+        case 'add':
+            todo = input('enter a todo:')
+            todos.append(todo)
+        case 'show':
+            for index, todo in enumerate(todos, start=1):
+                print(f"{index}-{todo.title()}")
+        case 'edit':
+            to_do_id = int(input('enter a id to edit: '))
+            todo  = input('enter new todo:')
+            todos[to_do_id - 1] = todo
+        case 'exit':
+            break
